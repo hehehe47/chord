ff51fd8c3f748134703f927a589da01c51037f9f
Zepei Xu <xuzepei19950617@gmail.com>
2019-10-17 18:39:02 -0400

    mod

--- src/Chord/ChordView/index.js
+++ src/Chord/ChordView/index.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class ChordView extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      chordConfig: {},
+      nodesConfig: {}
+    };
   }
 
   render() {
